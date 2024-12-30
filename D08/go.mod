9b0c95f0c19ad72cd9954969b823d77dab90b15a
Tobias Baumgartl <tobias.baumgartl@outlook.de>
2024-12-30 01:40:32 +0100

    Part1 working so far

--- D08/go.mod
+++ D08/go.mod
@@ -1,3 +1,8 @@
 module github.com/CXNNIBVL/AdventOfCode24/D08
 
 go 1.23.4
+
+require (
+	github.com/CXNNIBVL/goutil v0.0.0-20241228002916-781d5fd26629 // indirect
+	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
+)
--- D08/main.go
+++ D08/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sync"
+
+	"github.com/CXNNIBVL/goutil/math"
 )
 
 const (
-	FILE      = "../inputs/D08/input"
-	TEST_FILE = "../inputs/D08/input_test"
+	FILE       = "../inputs/D08/input"
+	TEST_FILE  = "../inputs/D08/input_test"
+	TEST_FILE1 = "../inputs/D08/input_test1"
+	TEST_FILE2 = "../inputs/D08/input_test2"
+	TEST_FILE3 = "../inputs/D08/input_test3"
+	EN_DBG     = false
 )
 
 func readAllLines(f *os.File) []string {
@@ -22,9 +30,13 @@ func readAllLines(f *os.File) []string {
 	return lines
 }
 
+type Vec2 struct {
+	x, y int
+}
+
 type Frequency rune
 type Antenna struct {
-	x, y int
+	pos  Vec2
 	freq Frequency
 }
 
@@ -32,36 +44,91 @@ const (
 	OBJ_NOTHING rune = '.'
 )
 
-func parseAntennas(lines []string) []Antenna {
-	antennas := []Antenna{}
+func parseAntennas(lines []string) map[Frequency][]Vec2 {
+	m := make(map[Frequency][]Vec2)
 
 	for row, line := range lines {
 		rline := []rune(line)
 
-		for col, f := range rline {
-			if f == OBJ_NOTHING {
+		for col, r := range rline {
+
+			if r == OBJ_NOTHING {
 				continue
 			}
 
-			antennas = append(antennas, Antenna{x: col, y: row, freq: Frequency(f)})
+			f := Frequency(r)
+
+			if m[f] == nil {
+				m[f] = append([]Vec2{}, Vec2{x: col, y: row})
+			} else {
+				m[f] = append(m[f], Vec2{x: col, y: row})
+			}
 		}
 	}
 
-	return antennas
+	return m
 }
 
-type AntiNode struct {
-	x, y  int
-	freqs []Frequency
+type AntiNodeResult struct {
+	freq      Frequency
+	positions []Vec2
 }
 
-func calcAllAntiNodes(xbound, ybound int, antennas []Antenna) []AntiNode {
-	// TODO:
-	return nil
+func isPointInBound(v Vec2, xbound, ybound int) bool {
+	return v.x < xbound && v.x >= 0 && v.y < ybound && v.y >= 0
+}
+
+func findAntiNodePositions(freq Frequency, xbound, ybound int, antennaPoints []Vec2, ch chan<- AntiNodeResult, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	foundPositions := []Vec2{}
+
+	points := antennaPoints
+	for len(points) > 1 {
+		for ix := 1; ix < len(points); ix++ {
+			p1, p2 := points[0], points[ix]
+
+			if p1.x == p2.x && p1.y == p2.y {
+				continue
+			}
+
+			hdiff, vdiff := math.Abs(p1.x-p2.x), math.Abs(p1.y-p2.y)
+			anti1, anti2 := Vec2{x: -1, y: -1}, Vec2{x: -1, y: -1}
+			hflip, vflip := 1, 1
+
+			// Check if p1 is rigth of / below p2
+			if p1.x > p2.x || p1.y > p2.y {
+				// Flip addition direction
+				hflip = -1
+			}
+
+			if p1.y > p2.y {
+				// Flip addition direction
+				vflip = -1
+			}
+
+			// Note: The diagonal doesn't have to be a perfect diagonal
+			// like you would find going thru corner to corner of a square
+			anti1.y, anti2.y = p1.y-vflip*vdiff, p2.y+vflip*vdiff
+			anti1.x, anti2.x = p1.x-hflip*hdiff, p2.x+hflip*hdiff
+
+			if isPointInBound(anti1, xbound, ybound) {
+				foundPositions = append(foundPositions, anti1)
+			}
+
+			if isPointInBound(anti2, xbound, ybound) {
+				foundPositions = append(foundPositions, anti2)
+			}
+		}
+
+		points = points[1:]
+	}
+
+	ch <- AntiNodeResult{positions: foundPositions, freq: freq}
 }
 
 func main() {
-	f, err := os.Open(TEST_FILE)
+	f, err := os.Open(FILE)
 
 	if err != nil {
 		panic(err)
@@ -70,9 +137,54 @@ func main() {
 	defer f.Close()
 
 	lines := readAllLines(f)
-	antennas := parseAntennas(lines)
+	antennaMap := parseAntennas(lines)
 
 	xbound, ybound := len(lines[0]), len(lines)
 
-	antiNodes := calcAllAntiNodes(xbound, ybound, antennas)
+	if EN_DBG {
+		fmt.Println("### Antennas")
+		for k, va := range antennaMap {
+			fmt.Printf("--- Freq: %s ---\n", string(k))
+			for _, v := range va {
+				fmt.Printf("%+v\n", v)
+			}
+		}
+	}
+
+	ch := make(chan AntiNodeResult)
+	var wg sync.WaitGroup
+
+	for freq, antennaPoints := range antennaMap {
+		wg.Add(1)
+		go findAntiNodePositions(freq, xbound, ybound, antennaPoints, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	antiNodeMap := make(map[Vec2][]Frequency)
+
+	for r := range ch {
+		for _, p := range r.positions {
+			antiNodeMap[p] = append(antiNodeMap[p], r.freq)
+		}
+	}
+
+	if EN_DBG {
+		fmt.Println("### AntiNodes")
+		for p, freqs := range antiNodeMap {
+			sfreqs := string(freqs[0])
+			freqs = freqs[1:]
+
+			for _, r := range freqs {
+				sfreqs = sfreqs + ", " + string(r)
+			}
+
+			fmt.Printf("p: %+v, f: [%s]\n", p, sfreqs)
+		}
+	}
+
+	fmt.Println(len(antiNodeMap))
 }
