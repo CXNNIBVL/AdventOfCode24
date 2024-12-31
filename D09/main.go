3e556f551b3b100af769454038c0f72514d42f5b
Tobias Baumgartl <tobias.baumgartl@outlook.de>
2024-12-31 09:24:41 +0100

    testfile works- real file dont

---
+++ D09/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+
+	"github.com/CXNNIBVL/goutil/iter"
+)
+
+const (
+	FILE      = "../inputs/D09/input"
+	TEST_FILE = "../inputs/D09/input_test"
+)
+
+const (
+	FS_SPACE = -1
+)
+
+func parseFileSysAndDiskMap(f *os.File) (fs, dm []int) {
+	sc := bufio.NewScanner(f)
+
+	diskMap := ""
+
+	if ok := sc.Scan(); !ok {
+		panic("No line read")
+	} else {
+		diskMap = sc.Text()
+	}
+
+	isFile := true
+
+	fs, dm = []int{}, []int{}
+
+	id := 0
+	for _, numstr := range diskMap {
+		sizeFsItem, _ := strconv.ParseInt(string(numstr), 10, 64)
+
+		dm = append(dm, int(sizeFsItem))
+
+		descriptor := 0
+
+		if isFile {
+			descriptor = id
+			id = id + 1
+		} else {
+			descriptor = FS_SPACE
+		}
+
+		isFile = !isFile
+
+		for range iter.Interval(0, sizeFsItem) {
+			fs = append(fs, descriptor)
+		}
+	}
+
+	return fs, dm
+}
+
+func moveToNextSpace(fs []int) []int {
+	ix := slices.Index(fs, FS_SPACE)
+	return fs[ix:]
+}
+
+func moveToNextChunk(fs []int) []int {
+	for ix := len(fs) - 1; ix >= 0; ix-- {
+		if fs[ix] != FS_SPACE {
+			return fs[:ix+1]
+		}
+	}
+
+	return fs
+}
+
+func compressFS(fs []int) {
+	for len(fs) != 1 {
+		fs = moveToNextSpace(fs)
+		fs = moveToNextChunk(fs)
+
+		fs[0] = fs[len(fs)-1]
+		fs[len(fs)-1] = FS_SPACE
+	}
+}
+
+func calcFSChecksum(fs []int) int {
+	cs := 0
+
+	for ix, id := range fs {
+		if id == FS_SPACE {
+			break
+		}
+
+		cs = cs + ix*id
+	}
+
+	return cs
+}
+
+func main() {
+
+	f, err := os.Open(TEST_FILE)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	fs, _ := parseFileSysAndDiskMap(f)
+	// fmt.Println(dm)
+
+	compressFS(fs)
+	fmt.Println(calcFSChecksum(fs))
+}
